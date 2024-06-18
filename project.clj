4d578656137878217e0d4ee6ae47ff46f8bf360d
Timothy Pratley <timothypratley@gmail.com>
2024-06-17 22:11:18 -0700

    trying to make a snapshot lein

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject happyapi "0.1.0"
+(defproject happyapi "0.1.0-SNAPSHOT"
   :description "Middleware oriented oauth2 client for webservices"
   :url "http://github.com/timothypratley/happyapi"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"

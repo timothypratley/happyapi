d5a13f752269b9a51484176568bd27822aaed061
Timothy Pratley <timothypratley@gmail.com>
2024-06-17 22:06:49 -0700

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject happyapi "0.1.0-SNAPSHOT"
+(defproject happyapi "0.1.0"
   :description "Middleware oriented oauth2 client for webservices"
   :url "http://github.com/timothypratley/happyapi"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
